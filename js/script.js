706912c596ea2ef8e49100c87bfa0efe6f36ff73
Torben Schytt-Nielsen <tfsn86@gmail.com>
2019-02-06 19:54:09 +0100

    Added the t-shirt section and register for activity code

--- js/script.js
+++ js/script.js
@@ -50,6 +50,75 @@ Events at the same day and time should not be able to be checked at the same tim
 When the user checks activities information with the total costs should display beneath the checkboxes.
  */
 
-$('.activities').on('change', function (){
-    
+
+
+// checkbox input variables
+const $mainConfCheckBox = $('input[name="all"]');
+const $jsFramworksCheckBox = $('input[name="js-frameworks"]');
+const $jsLibrarysCheckBox = $('input[name="js-libs"]');
+const $expressCheckBox = $('input[name="express"]');
+const $nodeCheckBox = $('input[name="node"]');
+const $buildToolsCheckBox = $('input[name="build-tools"]');
+const $npmCheckBox = $('input[name="npm"]');
+
+$jsFramworksCheckBox.on('change', function () {
+    if ($(this).prop('checked')) {
+        $expressCheckBox.prop('disabled', true);
+        $expressCheckBox.parent().css('color', 'grey');
+    }   else if ($(this).prop('checked', false)) {
+        $expressCheckBox.prop('disabled', false);
+        $expressCheckBox.parent().css('color', '#000');
+    }
+});
+
+$jsLibrarysCheckBox.on('change', function () {
+    if ($(this).prop('checked')) {
+        $nodeCheckBox.prop('disabled', true);
+        $nodeCheckBox.parent().css('color', 'grey');
+    }   else if ($(this).prop('checked', false)) {
+        $nodeCheckBox.prop('disabled', false);
+        $nodeCheckBox.parent().css('color', '#000');
+    }
+});
+
+$expressCheckBox.on('change', function () {
+    if ($(this).prop('checked')) {
+        $jsFramworksCheckBox.prop('disabled', true);
+        $jsFramworksCheckBox.parent().css('color', 'grey');
+    }   else if ($(this).prop('checked', false)) {
+        $jsFramworksCheckBox.prop('disabled', false);
+        $jsFramworksCheckBox.parent().css('color', '#000');
+    }
+});
+
+$nodeCheckBox.on('change', function () {
+    if ($(this).prop('checked')) {
+        $jsLibrarysCheckBox.prop('disabled', true);
+        $jsLibrarysCheckBox.parent().css('color', 'grey');
+    }   else if ($(this).prop('checked', false)) {
+        $jsLibrarysCheckBox.prop('disabled', false);
+        $jsLibrarysCheckBox.parent().css('color', '#000');
+    }
+});
+
+
+/* A running total should display below the list of checkboxes. 
+A div for the total costs are created and appended to the register for activities section.
+When a checkbox is checked, the value of the label text (price) is stored in variables.
+If a checkbox is checked the running total is updated. 
+*/
+
+let $runningTotal = 0;
+let $costDiv = $('<div><span>Total Cost: $'+ $runningTotal + '</span></div>');
+$('.activities').append($costDiv);
+ 
+$('.activities').on('change', function(){
+    const $activityString = $(event.target).parent().text(); 
+    const $activityPrice = parseInt($activityString.substring($activityString.length-3));
+    if ($(event.target).is(':checked')) {
+        $runningTotal += $activityPrice;
+    } else { 
+        $runningTotal -= $activityPrice;
+    }
+    $costDiv.html('<div><span>Total Cost: $'+ $runningTotal + '</span></div>');
 });
